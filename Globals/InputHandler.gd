43ea9701ae3ab6dc660a3bf731017d1b91013dc6
samtabareh <sam.tabareh@gmail.com>
2025-01-23 00:37:07 +0330

    Init

--- Globals/InputHandler.gd
+++ Globals/InputHandler.gd
@@ -3,10 +3,10 @@ extends Main
 signal clicked
 
 func _input(event):
-	if Input.is_action_just_released("click"): clicked.emit()
+	#if Input.is_action_just_released("click"):
 	
-	if event is InputEventMouseMotion:
-		pass
+	#if event is InputEventMouseMotion:
+		#pass
 	
 	if Input.is_action_just_released("dev"):
 		Main.is_dev = !Main.is_dev
